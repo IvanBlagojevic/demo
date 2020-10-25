b0234811f7c690fe3f23f8b87f5baf5a80923773
IvanBlagojevic <30447633+IvanBlagojevic@users.noreply.github.com>
2020-10-25 13:55:37 +0100

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Runner.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Runner {
+
+//    @Autowired
+//    private SomeInterface someInterface;
+
+}
